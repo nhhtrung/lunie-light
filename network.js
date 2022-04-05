0a554a3eb12861aff411849db4924ffea691d9ea
azstake <75195495+nhhtrung@users.noreply.github.com>
2022-04-05 18:10:04 +0700

    Update network.js

--- network.js
+++ network.js
@@ -1,24 +1,24 @@
 export default {
-  id: 'cosmos-hub-testnet', // DEPRECATE, only used for Lunie extension, NOT CHAIN ID
-  name: 'Cosmos Stargate',
+  id: 'gravity-bridge', // DEPRECATE, only used for Lunie extension, NOT CHAIN ID
+  name: 'Gravity Bridge',
   description:
-    'Cosmos is a network of independent parallel blockchains, powered by BFT consensus algorithms like Tendermint.',
+    'Gravity Bridge',
   logo: `logo.svg`,
-  website: 'https://cosmos.network',
-  apiURL: 'http://localhost:8010/proxy', // use `npx lcp --proxyUrl http://34.123.30.100:1317`
-  rpcURL: 'ws://34.123.30.100:26657',
-  stakingDenom: 'MUON',
+  website: 'http://gravitybridge.net/',
+  apiURL: 'https://gravitychain.io:1317', // use `npx lcp --proxyUrl http://34.123.30.100:1317`
+  rpcURL: 'ws://gravitychain.io:26657',
+  stakingDenom: 'UGRAVITON',
   coinLookup: [
     {
-      viewDenom: 'MUON',
-      chainDenom: 'umuon',
+      viewDenom: 'UGRAVITON',
+      chainDenom: 'UGRAVITON',
       chainToViewConversionFactor: 1e-6,
       icon: `currencies/muon.png`,
     },
   ],
-  addressPrefix: 'cosmos',
-  validatorAddressPrefix: 'cosmosvaloper',
-  validatorConsensusaddressPrefix: 'cosmosvalcons', // needed to map validators from staking queries to the validator set
+  addressPrefix: 'gravity',
+  validatorAddressPrefix: 'gravityvaloper',
+  validatorConsensusaddressPrefix: 'gravityvalcons', // needed to map validators from staking queries to the validator set
   HDPath: `m/44'/118'/0'/0/0`,
   lockUpPeriod: `3 days`,
   fees: {
@@ -26,8 +26,8 @@ export default {
       gasEstimate: 350000,
       feeOptions: [
         {
-          denom: 'MUON',
-          amount: 0.001,
+          denom: 'UGRAVITON',
+          amount: 0.000,
         },
       ],
     },
